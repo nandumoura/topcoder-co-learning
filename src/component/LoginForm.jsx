3ef0d547f44515df4101815c6a099afc0fb6bd46
Fernando Moura <nandumoura@gmail.com>
2023-03-24 08:30:06 -0300

    a

--- src/component/LoginForm.jsx
+++ src/component/LoginForm.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import auth from "../firebase-config";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import {
   FormControl,
   FormLabel,
