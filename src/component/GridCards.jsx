814a7f7f0d91eaaa77cf0eb6e95a6fb63db7d604
Fernando Moura <nandumoura@gmail.com>
2023-03-25 20:26:16 -0300

    fix: get data

--- src/component/GridCards.jsx
+++ src/component/GridCards.jsx
@@ -1,25 +1,48 @@
-import data from "../utils/data";
+import React, { useState, useEffect } from "react";
 
+import { getAllLearningSpaces } from "../firebase-config";
 import { Box, Heading, Grid } from "@chakra-ui/react";
 // my componentes
 import CourseCard from "./CourseCard";
 
 const GridCards = () => {
+  const [learningSpaces, setLearningSpaces] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  async function testGetAllLearningSpaces() {
+    const learningSpaces = await getAllLearningSpaces([]);
+    console.log(learningSpaces[0].updated_at);
+    setLearningSpaces(learningSpaces);
+    setIsLoading(false);
+  }
+
+  useEffect(() => {
+    testGetAllLearningSpaces();
+  }, []);
+  if (isLoading) {
+    return <>Loading</>;
+  }
+  function formatData(obj) {
+    const updatedAt = new Date(obj.seconds * 1000);
+    const formattedDate = `${updatedAt.toLocaleDateString()} ${updatedAt.toLocaleTimeString()}`;
+    return formattedDate;
+  }
   return (
     <Box display="grid" gridGap={2} gridAutoFlow="row dense">
       <Heading size="md" textAlign={"center"}>
-        {" "}
         Co-learning Spaces section
       </Heading>
       <Grid templateColumns="repeat(3, 1fr)" gap={6} padding={4}>
-        {data.map((node) => {
+        {learningSpaces.map((space) => {
+          console.log(space);
           return (
             <CourseCard
-              key={node.node.youtubePreviewId}
-              name={node.node.courseTitle}
-              updatedAt={node.node.createdAt}
-              imgSrc={node.node.courseImage.gatsbyImageData.images.fallback.src}
-              courseUrl={node.node.courseUrl}
+              key={space.id}
+              name={space.name}
+              updatedAt={formatData(space.updated_at)}
+              imgSrc={space.thumbnail}
+              courseUrl={"/"}
+              people={space?.users?.length}
             />
           );
         })}
