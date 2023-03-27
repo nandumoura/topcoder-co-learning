fe121e538f6d0e82696395aa6b9eef74450f17f0
Fernando Moura <nandumoura@gmail.com>
2023-03-26 21:52:14 -0300

    remove unused code and add provider redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-
+import store from "./store";
+import { Provider } from "react-redux";
 import { ChakraProvider } from "@chakra-ui/react";
 
 // components
@@ -9,7 +10,9 @@ import App from "./App";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </React.StrictMode>
 );
