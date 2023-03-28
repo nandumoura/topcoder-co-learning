46af06099626776c92f2ba92df1bed317295edbc
Fernando Moura <nandumoura@gmail.com>
2023-03-27 21:16:10 -0300

    change reducers

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "../redux/userSlice";
-import learningSpacesSlice from "../redux/learningSpacesSlice";
 
 export default configureStore({
   reducer: {
     user: userSlice,
-    learningSpaces: learningSpacesSlice,
   },
 });
